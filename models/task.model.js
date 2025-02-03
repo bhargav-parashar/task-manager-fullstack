ec3bb2d0eb7f48af16ec050d91af2a58e9bb79e8
Bhargav Parashar <bhargavparashar99@gmail.com>
2025-02-03 23:54:31 +0530

    feat:multer to store uploaded files

--- models/task.model.js
+++ models/task.model.js
@@ -5,6 +5,10 @@ const taskSchema = new mongoose.Schema(
     title: {type:String, required:true},
     description : {type:String, required:true},
     status : {type:String, enum: [ 'TODO', 'DONE' ], default:'TODO'},
+    linkedFile : {
+        data : Buffer,
+        contentType : String
+    },
     deadline : {type:Date, required:true }
 },
 {
--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,14 +1,15 @@
-const {
+  const {
     getTasks,
     createTask,
     updateTask,
     deleteTask,
   } = require("../controllers/task.controllers");
-  
+
+  const upload = require("../config/multerConfig");
   const router = require("express").Router();
   
   router.get("/", getTasks);
-  router.post("/", createTask);
+  router.post("/", upload.single("pdf"), createTask);
   router.patch("/:id", updateTask);
   router.delete("/:id", deleteTask);
   
